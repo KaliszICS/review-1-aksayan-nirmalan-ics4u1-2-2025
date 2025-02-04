24c0979024aadefdb3d4d93b636e0f2ebf395062
aksayan nirmalan-ics4u1-2 <aksayan.nirmalan@student.tdsb.on.ca>
2025-02-04 16:47:51 +0000

    save changes

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,28 +2,45 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
-		System.out.println("There was a man from St.Ives");
-		
+		System.out.println("There once was a man from St. Ives.");
+	
+	
 	}
 
 	public static void q2() {
 		//Write question 2 code here
-		int f=5;
 		int n=9;
-		System.out.println(f-f);
-		System.out.println(n-f);
+		int f=5;
+		long b= Math.round(n/f);
+		long a= n%f;
+		System.out.println(b);
+		System.out.println(a);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		
+		//System.out.println("Hello");
+		//System.out.println("Hello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		int x= 8;
+		int y=20;
+
+		if(x > y)
+		{
+			System.out.println("false");
+		}
+		else
+		System.out.println("true");
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		System.out.print("Hello");
+		System.out.println(" World");
 	}
 
 }
