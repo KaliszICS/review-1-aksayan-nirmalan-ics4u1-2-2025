8178e4efbd18f191e4add71dae29bc9fa7d708cd
aksayan nirmalan-ics4u1-2 <aksayan.nirmalan@student.tdsb.on.ca>
2025-02-04 15:22:28 +0000

    save changes

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,10 +2,16 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There was a man from St.Ives");
+		
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int f=5;
+		int n=9;
+		System.out.println(f-f);
+		System.out.println(n-f);
 	}
 
 	public static void q3() {
